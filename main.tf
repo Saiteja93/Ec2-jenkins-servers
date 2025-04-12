5ac8b3a88bdcad25ce8ec70544cae14d54ab6ac7
Saiteja93 <saitejaraj.guvvala@gmail.com>
2025-04-11 23:55:39 -0700

    8080port

--- main.tf
+++ main.tf
@@ -1,4 +1,40 @@
 
+# Security Group to allow SSH and port 8080
+resource "aws_security_group" "allow_ssh_master" {
+  name        = "allow_ssh_master"
+  description = "Allow SSH inbound traffic and port 8080 for master"
+  # vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # ⚠️ For testing only — lock to your IP for security
+  }
+
+    ingress {
+    description = "jenkins port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # ⚠️ For testing only — lock to your IP for security
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow ssh and jenkins port"
+  }
+}
+
+
 # Security Group to allow SSH
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
@@ -12,6 +48,8 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # ⚠️ For testing only — lock to your IP for security
   }
+
+
 
   egress {
     from_port   = 0
@@ -43,7 +81,7 @@ resource "aws_instance" "terraform-two" {
   instance_type               = var.instance_type_agent
   # subnet_id                   = data.aws_subnet_ids.default.ids[0]
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_master.id]
 
   tags = {
     Name = "jenkins-agent"
